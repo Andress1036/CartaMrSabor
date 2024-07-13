e3ab562ae617051fcfb09b629d5269a2fe29c3e0
Andres Cortes <edinsoncortes1@gmail.com>
2024-07-12 23:26:26 -0500

    que se muestre los menus desde el principio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { DesayunoComponent } from './desayuno/desayuno.component';
 export const routes: Routes = [
   {path: 'menus', component: MenusComponent},
   {path: 'especiales', component: EspecialesComponent},
-  {path: 'desayuno', component: DesayunoComponent}
+  {path: 'desayuno', component: DesayunoComponent},
+  {path: '', component: MenusComponent}
 ];
