400eceb049a5269a9ebd62df162ffcda46323b42
Andres Cortes <138270054+Andress1036@users.noreply.github.com>
2024-08-12 19:05:36 -0500

    ocultar barra con el scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MenusComponent } from "./menus/menus.component";
 import { EspecialesComponent } from './especiales/especiales.component';
@@ -17,6 +17,22 @@ export class AppComponent{
       top: 0,
       behavior: 'smooth'
     });
+  }
+
+  prevScrollpos = window.scrollY;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollPos = window.scrollY;
+    const navbar2 = document.getElementById('navbar2');
+
+   
+    if (this.prevScrollpos > currentScrollPos) {
+      if (navbar2) navbar2.style.top = '0';
+    } else {
+      if (navbar2) navbar2.style.top = '-70px'; // Ajusta según la altura de tu navbar
+    }
+    this.prevScrollpos = currentScrollPos;
   }
 
 }
